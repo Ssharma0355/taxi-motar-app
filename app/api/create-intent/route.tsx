7ac69ecaa8eaea60cd2a7639f312a917d3714596
sachin_sde <ssachin.s@myclnq.co.in>
2024-10-06 16:28:33 +0530

    second

--- app/api/create-intent/route.tsx
+++ app/api/create-intent/route.tsx
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe"; // Import from 'stripe' for backend use
 
 // Initialize Stripe instance with the secret key
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY,{
+
   apiVersion: "2024-09-30.acacia",
   typescript: true,
 });
