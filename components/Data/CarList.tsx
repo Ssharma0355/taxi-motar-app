97ab357da39b91ba244e3bbfe89bf165cc3e62a3
sachin_sde <ssachin.s@myclnq.co.in>
2024-10-08 20:08:39 +0530

    img

--- components/Data/CarList.tsx
+++ components/Data/CarList.tsx
@@ -1,26 +1,31 @@
+import cab from '../../Public/cab.png'
+import moto from "../../Public/moto.png";
+import luxary from "../../Public/luxary.png";
+import auto from "../../Public/auto.png";
+
 export default [
   {
     id: 1,
     name: "Economy",
-    image: "/cab.png",
+    image: cab,
     charges: 100,
   },
   {
     id: 2,
     name: "Auto",
-    image: "/auto.png",
+    image: auto,
     charges: 80,
   },
   {
     id: 3,
     name: "Bike",
-    image: "/moto.png",
+    image: moto,
     charges: 40,
   },
   {
     id: 3,
     name: "Luxary",
-    image: "/luxary.png",
+    image: luxary,
     charges: 250,
   },
 ];
--- components/Data/CardList.tsx
+++ components/Data/CardList.tsx
@@ -1,27 +1,35 @@
+import card from '../../Public/card.png'
+import visa from '../../Public/visa.png'
+import apple from "../../Public/apple-pay.png";
+import google from "../../Public/google-pay.png";
+import cash  from "../../Public/cash-on-delivery.png";
+
+
+
 export default [
   {
     id: 1,
     name: "Master Card",
-    image: "/card.png",
+    image: card,
   },
   {
     id: 1,
     name: "Visa Card",
-    image: "/visa.png",
+    image: visa,
   },
   {
     id: 1,
     name: "Apple Pay",
-    image: "/apple-pay.png",
+    image: apple,
   },
   {
     id: 1,
     name: "Google Pay",
-    image: "/google-pay.png",
+    image: google,
   },
   {
     id: 1,
     name: "Pay on Cash",
-    image: "/cash-on-delivery.png",
+    image: cash,
   },
 ];
